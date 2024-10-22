b113a940fdd3b004e5e9dc5b39e3f5859d83b390
joska <jpotin@gmail.com>
2024-10-22 16:04:44 +0200

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
+  site: "https://joska-p.github.io",
   base: "/sleepy-star",
+  integrations: [tailwind()],
 })
