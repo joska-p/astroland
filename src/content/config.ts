36683c9a6ec8d5724b7dc3ebd307b09273977b05
joska <jpotin@gmail.com>
2024-10-23 17:31:48 +0200

    add generated content and url

---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+// Example: A cheatsheet of many common Zod datatypes
+import { z, defineCollection } from "astro:content"
+
+const painting = z.object({
+  id: z.string(),
+  filename: z.string(),
+  title: z.string(),
+  year: z.number(),
+  height: z.number(),
+  width: z.number(),
+  slug: z.string(),
+  visible: z.boolean(),
+  featured: z.boolean(),
+  imageWidth: z.number(),
+  imageHeight: z.number(),
+  thumbnailWidth: z.number(),
+  thumbnailHeight: z.number(),
+  imageBlurhash: z.string(),
+  squareBlurhash: z.string(),
+  order: z.number(),
+})
+
+const paintings = defineCollection({
+  type: "data",
+  schema: painting,
+})
+
+export const collections = {
+  paintings: paintings,
+}
