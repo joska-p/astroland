bbf8b116129fa2e00eb81d0603b2a2ef03898ab3
joska <jpotin@gmail.com>
2024-12-07 18:27:03 +0100

    move link to separate file and add cta

---
+++ src/lib/navigation.ts
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    text: "Accueil",
+    href: "/",
+  },
+  {
+    text: "Peinture",
+    href: "/painting/1",
+  },
+  {
+    text: "À propos",
+    href: "/about",
+  },
+]
