96fa8b6b5c93c9fa107078c662bc10c7a70d0c07
joska <jpotin@gmail.com>
2024-12-13 11:53:55 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,26 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
+import vercel from "@astrojs/vercel"
+
 export default defineConfig({
   site: "https://astroland-sigma.vercel.app",
   output: "static",
   integrations: [tailwind()],
   compressHTML: false,
+
   prefetch: {
     defaultStrategy: "load",
     prefetchAll: true,
   },
+
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    maxDuration: 8,
+    imageService: true,
+    imagesConfig: {
+      sizes: [400, 800, 1200],
+      domains: [],
+    },
+  }),
 })
