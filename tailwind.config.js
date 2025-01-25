c13fbfb486a3408fc6688954f9ed0e06de626fff
joska <jpotin@gmail.com>
2025-01-25 20:24:19 +0100

    2 step. remove tailwind fluid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,15 @@
-import fluid, { extract, fontSize, screens } from "fluid-tailwind"
 import typography from "@tailwindcss/typography"
 
 export default {
   content: {
     files: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    extract,
   },
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "0.5rem",
-        sm: "1 rem",
-        lg: "1.5 rem",
-        xl: "2 rem",
-        "2xl": "2.5 rem",
-      },
-    },
-    screens,
-    fontSize,
     extend: {
       fontFamily: {
         daniel: ["Daniel", "cursive"],
       },
     },
   },
-  plugins: [typography, fluid],
+  plugins: [typography],
 }
