65d235fe67f66cf82de0eb3803e32ab774778ada
joska <jpotin@gmail.com>
2025-02-03 20:16:22 +0100

    try env for baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
+const BASE_URL = import.meta.env.BASE_URL
 
 export default defineConfig({
   site: "https://joska-p.github.io",
-  base: "/astroland",
+  base: BASE_URL,
   output: "static",
   compressHTML: false,
   prefetch: {
