eab308dbdd9a1fa7b7ed3235f7a8e2daf6c8517d
joska <jpotin@gmail.com>
2025-02-04 00:18:26 +0100

    use loadEnv in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
-import process from "node:process"
+import { loadEnv } from "vite"
+import process from "process"
+const { SITE_URL, BASE_URL } = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "")
 
 export default defineConfig({
-  site: process.env.SITE,
-  base: process.env.BASE,
+  site: SITE_URL,
+  base: BASE_URL,
   trailingSlash: "never",
   output: "static",
   compressHTML: false,
