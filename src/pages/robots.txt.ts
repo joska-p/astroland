be669c02f8a0ef8107c40c258ac04700951d8ecf
joska <jpotin@gmail.com>
2025-02-10 11:42:47 +0100

    Disallow indexation in robot txt if SITE_URL is not rolandpotin.com

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,22 @@
 import type { APIRoute } from "astro"
 
-const getRobotsTxt = (sitemapURL: URL) => `
+const environment = import.meta.env.SITE === "https://www.potineizner.com" ? "production" : "development"
+
+let getRobotsTxt
+
+if (environment === "production") {
+  getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
 `
+} else {
+  getRobotsTxt = () => `
+User-agent: *
+Disallow: /
+`
+}
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL("sitemap-index.xml", site)
